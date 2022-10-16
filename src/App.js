f6242c5310d56ad4d604071c30cf65648b2b2a08
LucaSain <lucasainenco2@gmail.com>
2022-10-16 19:28:58 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="grid place-content-center h-screen">
+      <div className="card w-96 bg-base-100 shadow-xl">
+        <figure className="hover:animate-pulse">
+          <img src={logo} className="mt-10"/>
+        </figure>
+        <div className="card-body mockup-code mt-10 bg-inherit border-4 border-emerald-600">
+          <pre data-prefix="$"><code>npm install PC4</code></pre>
+        </div>
+      </div>
+      </div>
+      
     </div>
   );
 }
